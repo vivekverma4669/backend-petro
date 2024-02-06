398baad21fb2ddc6cd8a8b520b700396af1d171b
vivekverma4669 <vivekverma4679@gmail.com>
2024-02-06 12:07:10 +0530

    backend

--- configs/db.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-// require('dotenv').config();
-
-const connection = mongoose.connect(`mongodb+srv://viveksonitech:8564910720@cluster0.z7gof.mongodb.net/petrol`);
-module.exports= {connection};
